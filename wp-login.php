08195a82be06dad1440d4e0a3fc34a79a12efe50
VitorPrates <111801858+VitorPrates@users.noreply.github.com>
2023-05-23 17:10:57 -0300

    correção teste inicial - index falho
    o index no commit anterior falhou, esse em tese o corrige

--- wp-login.php
+++ wp-login.php
@@ -4,12 +4,12 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Acessar &lsaquo; Vendas.Maveric &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
-<link rel="stylesheet" id="dashicons-css" href="https://github.com/VitorPrates/Maveric.AD/wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://github.com/VitorPrates/Maveric.AD/wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://github.com/VitorPrates/Maveric.AD/wp-admin/css/forms.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://github.com/VitorPrates/Maveric.AD/wp-admin/css/l10n.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="login-css" href="https://github.com/VitorPrates/Maveric.AD/wp-admin/css/login.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="microthemer-css" href="https://github.com/VitorPrates/Maveric.AD/wp-content/micro-themes/active-styles.css?mts=170#038;ver=6.2.2" media="all">
+<link rel="stylesheet" id="dashicons-css" href="./wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="./wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="forms-css" href="./wp-admin/css/forms.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="./wp-admin/css/l10n.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="login-css" href="./wp-admin/css/login.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="microthemer-css" href="./wp-content/micro-themes/active-styles.css?mts=170#038;ver=6.2.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -42,12 +42,12 @@
 		</form>
 
 					<p id="nav">
-				<a href="https://github.com/VitorPrates/Maveric.AD/wp-login.php?action=lostpassword">Perdeu a senha?</a>			</p>
+				<a href="./?action=lostpassword">Perdeu a senha?</a>			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="https://github.com/VitorPrates/Maveric.AD/">&larr; Ir para Vendas.Maveric</a>		</p>
+			<a href="./index.html">&larr; Ir para Vendas.Maveric</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
@@ -67,15 +67,15 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 
 					</form>
 				</div>
-				<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"https:\/\/github.com\/VitorPrates\/Maveric.AD\/wp-includes\/js\/zxcvbn.min.js"};</script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+				<script src="./wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
+<script src="./wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":".\/\/wp-includes\/js\/zxcvbn.min.js"};</script>
+<script src="./wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="./wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script src="./wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script src="./wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="./wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script src="./wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -85,17 +85,17 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2023-03-28 21:15:49+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"pt_BR"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s está obsoleta desde a versão %2$s. Use %3$s como alternativa. Considere escrever um código mais inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script src="./wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
+<script src="./wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/paginavendas\/wordpress\/wp-admin\/admin-ajax.php"}};</script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
+<script src="./wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"30e797b9d0"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2023-03-28 21:15:49+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"pt_BR"},"Your new password has not been saved.":["Sua nova senha não foi salva."],"Hide":["Esconder"],"Show":["Mostrar"],"Confirm use of weak password":["Confirmar o uso de uma senha fraca"],"Hide password":["Ocultar senha"],"Show password":["Mostrar senha"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
-<script src="https://github.com/VitorPrates/Maveric.AD/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
+<script src="./wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
