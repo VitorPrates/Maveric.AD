33f384175a103a0464cc21808455318e2ee7c946
VitorPrates <111801858+VitorPrates@users.noreply.github.com>
2023-08-01 11:51:04 -0300

    botões Yampi atualizados
    Problema de carrinho vazio resolvido, redirecionamento criado na yampi

--- wp-login.php
+++ wp-login.php
@@ -88,7 +88,7 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 <script src="./wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/paginavendas\/wordpress\/wp-admin\/admin-ajax.php"}};</script>
 <script src="./wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"54c4be3dd4"};</script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"5a3af30250"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
